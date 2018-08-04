16a41f604723c924218097b27a20d3dea9bdd160
Christopher Wells <cwells@anaxexp.io>
2018-08-04 10:28:48 -0700

    Update: image repos

--- base-php.sh
+++ base-php.sh
@@ -6,10 +6,10 @@ set -e
 
 git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/anaxexp/base-php" /tmp/base-php
 cd /tmp/base-php
-git remote add upstream https://github.com/docker-library/php
+git remote add origin https://github.com/docker-library/php
 #git fetch upstream
-git pull upstream master --allow-unrelated-histories
-git merge --strategy-option ours --no-edit upstream upstream/master
+git pull origin master --allow-unrelated-histories
+git merge --strategy-option ours --no-edit origin origin/master
 
 ./anaxexp-meta-update.sh
 
