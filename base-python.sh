8edd7f1ccf958c0f0cf2676b10dbea1fc5eb9c31
AnaxExp Robot <vagrant@anax-laptop.localdomain>
2018-08-04 14:05:38 -0700

    init premissions commit

--- base-python.sh
+++ base-python.sh
@@ -7,8 +7,8 @@ set -e
 git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/anaxexp/base-python" /tmp/base-python
 cd /tmp/base-python
 git remote add upstream https://github.com/docker-library/python
-git fetch upstream -f
-git merge --strategy-option ours --no-edit upstream/master
+git pull upstream master -f --allow-unrelated-histories
+git merge upstream --strategy-option ours --no-edit  upstream/master
 
 ./anaxexp-meta-update.sh
 
