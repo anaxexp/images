97969833c74c18f4a638d00b68709fcb730fe270
AnaxExp Robot <vagrant@anax-laptop.localdomain>
2018-08-04 22:45:11 -0700

    update: build versions

--- base-ruby.sh
+++ base-ruby.sh
@@ -6,8 +6,8 @@ set -e
 
 git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/anaxexp/base-ruby" /tmp/base-ruby
 cd /tmp/base-ruby
-git remote add upstream https://github.com/docker-library/ruby
-git fetch upstream -f
+git remote add upstream https://github.com/docker-library/ruby --fetch
+git fetch upstream --force
 git merge --strategy-option ours --no-edit upstream/master
 
 ./anaxexp-meta-update.sh
--- httpd.sh
+++ httpd.sh
@@ -6,8 +6,8 @@ set -e
 
 git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/anaxexp/httpd" /tmp/httpd
 cd /tmp/httpd
-git remote add upstream https://github.com/docker-library/httpd
-git fetch upstream -f
+git remote add upstream https://github.com/docker-library/httpd --fetch
+git fetch upstream --force
 git merge --strategy-option ours --no-edit upstream/master
 
 ./anaxexp-meta-update.sh
