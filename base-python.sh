4ea7999182a357c5f75c098b13ed876342e49773
AnaxExp Robot <vagrant@anax-laptop.localdomain>
2018-08-06 12:17:53 -0700

    update: fixes

--- base-python.sh
+++ base-python.sh
@@ -6,9 +6,9 @@ set -e
 
 git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/anaxexp/base-python" /tmp/base-python
 cd /tmp/base-python
+git rebase
 git remote add upstream https://github.com/docker-library/python --fetch
 git fetch upstream --force
-git rebase
 git merge --strategy-option ours --no-edit  upstream/master
 
 ./anaxexp-meta-update.sh
